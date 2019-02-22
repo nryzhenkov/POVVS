4374101b979ee1981b5a9e9504a028871efe0f45
nryzhenkov <nryzhenkov@yandex.ru>
2019-02-22 22:59:09 +0300

    add socket

--- lab2/client.c
+++ lab2/client.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
+
+	int a = 1337;
 
 	if (argc < 2)
 	{
@@ -14,9 +19,24 @@ int main(int argc, char* argv[])
 
 	if (sockid < 0)
 	{
-		printf("Ошибка создания сокета");
+		printf("Ошибка создания сокета\n");
 		return -1;
 	}
+
+	struct sockaddr_in addr;
+
+	addr.sin_family = AF_INET;
+	addr.sin_port   = htons(1337);
+	inet_aton("127.0.0.1", &addr.sin_addr);	
+	
+	if (connect(sockid, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+	{
+		printf("Ошибка соединения\n");
+		return -1;
+	}
+	
+	write(sockid, &a, sizeof(a));
+	close(sockid);
 
 	return 0;
 }
